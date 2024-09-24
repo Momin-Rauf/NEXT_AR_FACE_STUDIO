a1533cb352abf28703690ec7f49b1108671947d0
Momin-Rauf <mrauf.bese21seecs@seecs.edu.pk>
2024-09-25 02:27:41 +0500

    resolving issues

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      // Adding a rule to handle .glb and .gltf files
+      config.module.rules.push({
+        test: /\.(glb|gltf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
