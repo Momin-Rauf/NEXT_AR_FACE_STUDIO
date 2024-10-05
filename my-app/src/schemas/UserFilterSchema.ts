82ecb43f016c530ba0ca256f94ebe77c7161459d
Momin-Rauf <mrauf.bese21seecs@seecs.edu.pk>
2024-10-05 14:14:27 +0500

    added zod for validation

---
+++ my-app/src/schemas/UserFilterSchema.ts
@@ -0,0 +1,6 @@
+import {z} from "zod"
+
+export const UserFilterSchema = z.object({
+    image_url: z.string(),
+    model_url:z.string(),
+})
---
+++ my-app/src/schemas/signUpSchema.ts
@@ -0,0 +1,12 @@
+import {z} from 'zod';
+
+export const usernameValidation = z
+    .string()
+    .min(2,"Must me atleast 2 characters")
+    .max(20,"Must not be more than 2 characters")
+
+export const signUpSchema = z.object({
+    username:usernameValidation,
+    email:z.string().email({message:"invalid email address"}),
+    password: z.string().min(6,{message:"password must be atleast 6 chars"})
+})
