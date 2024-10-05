b2057f17cf4e34fb7aa7a7454766bd1428aef5f2
Momin-Rauf <mrauf.bese21seecs@seecs.edu.pk>
2024-10-05 14:38:28 +0500

    Database connection using mongoose

--- my-app/src/lib/dbConnect.ts
+++ my-app/src/lib/dbConnect.ts
@@ -18,7 +18,7 @@ async function dbConnect():Promise<void> {
         console.log('DB connected')
     }
     catch(error){
-        console.log('db connection failed')
+        console.log('db connection failed',error)
         process.exit(1);
     }
 }
