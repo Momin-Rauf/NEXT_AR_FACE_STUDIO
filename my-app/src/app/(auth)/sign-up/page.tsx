3ea7396a2be83838337971f2ca35577669d39b43
Momin-Rauf <mrauf.bese21seecs@seecs.edu.pk>
2024-10-14 09:12:36 +0500

    update

--- my-app/src/app/(auth)/sign-up/page.tsx
+++ my-app/src/app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import axios from 'axios';
-import {ApiResponse} from '@/types/ApiResponse';
+
 
 import {
   Form,
