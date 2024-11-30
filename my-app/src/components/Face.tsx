72dd405d94e4f934b93378e2b342c93aed7881e9
Momin-Rauf <mrauf.bese21seecs@seecs.edu.pk>
2024-11-30 11:13:10 +0500

    error debuggin

--- my-app/src/components/Face.tsx
+++ my-app/src/components/Face.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import { useGLTF } from '@react-three/drei';
+import React from 'react'
 
-export function Face(props) {
-  const { nodes, materials } = useGLTF('/face_model.glb');
-
-  // TypeScript type assertion to tell TypeScript that nodes.Object_2 is a mesh
-  const meshNode = nodes['Object_2'] as THREE.Mesh;
-
+const Face = () => {
   return (
-    <group {...props} dispose={null}>
-      <mesh
-        castShadow
-        receiveShadow
-        scale={[0.01, 0.01, 0.01]}  // Scale adjusted to be smaller
-        position={[0, 0, 0]}  
-        geometry={meshNode.geometry}  // Access geometry on the mesh
-        material={materials['04___Default']}
-        rotation={[-Math.PI / 2, 0, 0]}
-      />
-    </group>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-useGLTF.preload('/face_model.glb');
+export default Face
